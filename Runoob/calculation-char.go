048c722b0225add271f8e487e58baeffa479c9ea
TangJ <86515447+rjk13637110731@users.noreply.github.com>
2023-09-12 09:41:21 +0800

    Create calculation-char.go

---
+++ Runoob/calculation-char.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a int = 21
+	var b int = 10
+	var c int
+
+	c = a + b
+	fmt.Printf("第一行-c的值为 %d\n", c)
+	c = a - b
+	fmt.Printf("第二行-c的值为 %d\n", c)
+	c = a * b
+	fmt.Printf("第三行-c的值为 %d\n", c)
+	c = a / b
+	fmt.Printf("第四行-c的值为 %d\n", c)
+	c = a % b
+	fmt.Printf("第五行-c的值为 %d\n", c)
+	a++
+	fmt.Printf("第六行-a的值为 %d\n", a)
+	a--
+	fmt.Printf("第六行-a的值为 %d\n", a)
+}
