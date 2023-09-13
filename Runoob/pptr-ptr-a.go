054eecc6b2d800ac9a60584e6d4b49750035e770
TangJ <86515447+rjk13637110731@users.noreply.github.com>
2023-09-13 20:28:53 +0800

    Create pptr-ptr-a.go

---
+++ Runoob/pptr-ptr-a.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a int
+	var ptr *int
+	var pptr **int
+	a = 3000
+
+	ptr = &a
+	pptr = &ptr
+
+	fmt.Printf("a = %d\n", a)
+	fmt.Printf("*ptr = %d\n", *ptr)
+	fmt.Printf("*pptr = %d\n", **pptr)
+}
