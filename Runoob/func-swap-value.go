c82d9063aed103470b11da202b80fb8793136505
TangJ <86515447+rjk13637110731@users.noreply.github.com>
2023-09-13 13:38:00 +0800

    Update func-swap-value.go

--- Runoob/func-swap-value.go
+++ Runoob/func-swap-value.go
@@ -1,1 +1,21 @@
+package main
 
+import "fmt"
+
+func main() {
+	var a int = 100
+	var b int = 200
+	fmt.Printf("交互前a的值：%d\n", a)
+	fmt.Printf("交换前b的值：%d\n", b)
+	swap(a, b)
+	fmt.Printf("交互后a的值：%d\n", a)
+	fmt.Printf("交换后b的值：%d\n", b)
+}
+
+func swap(x, y int) int {
+	var temp int
+	temp = x
+	x = y
+	y = temp
+	return temp
+}
