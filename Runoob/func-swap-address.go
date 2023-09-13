44aa72d64bcf0641331224f3712dd87f124c17ab
TangJ <86515447+rjk13637110731@users.noreply.github.com>
2023-09-13 13:39:56 +0800

    Create func-swap-address.go

---
+++ Runoob/func-swap-address.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a int = 100
+	var b int = 200
+	fmt.Printf("交互前a的值：%d\n", a)
+	fmt.Printf("交换前b的值：%d\n", b)
+	swap(&a, &b)
+	fmt.Printf("交互后a的值：%d\n", a)
+	fmt.Printf("交换后b的值：%d\n", b)
+}
+
+func swap(x *int, y *int) {
+	var temp int
+	temp = *x
+	*x = *y
+	*y = temp
+}
