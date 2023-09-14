34fc900cb178265681b2ec843f020d55f5666d89
TangJ <86515447+rjk13637110731@users.noreply.github.com>
2023-09-14 17:04:41 +0800

    Create string-interface.go

---
+++ Runoob/string-interface.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var i interface{} = "Hello World!"
+	str, ok := i.(string)
+	if ok {
+		fmt.Printf("'%s' is a string\n", str)
+	} else {
+		fmt.Println("Conversion Failed")
+	}
+}
